6614ccea8f2569bfe10ff10b34971f8583b17b42
Okan Kırık <okankirik@gmail.com>
2024-11-26 22:41:04 +0300

    Deneyim crud işlemleri tamamlandı.

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,11 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebsite.DAL.Context;
+using PersonalWebsite.DAL.Entities;
 
 namespace PersonalWebsite.Controllers;
 
 public class ExperienceController : Controller
 {
+    private readonly PersonalWebsiteContext _context;
+
+    public ExperienceController(PersonalWebsiteContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult ExperienceList()
+    {
+        var values = _context.Experiences.ToList();
+        return View(values);
+    }
+
+    [HttpGet]
+    public IActionResult CreateExperience()
     {
         return View();
     }
+
+	[HttpPost]
+	public IActionResult CreateExperience(Experience experience)
+	{
+        _context.Experiences.Add(experience);
+        _context.SaveChanges();
+		return RedirectToAction("ExperienceList");
+	}
+
+    
+    public IActionResult DeleteExperience(int id) 
+    {
+        var value =  _context.Experiences.Find(id);
+        _context.Experiences.Remove(value);
+        _context.SaveChanges();
+		return RedirectToAction("ExperienceList");
+	}
+
+    [HttpGet]
+    public IActionResult UpdateExperience(int id)
+    {
+        var value = _context.Experiences.Find(id);
+        return View(value);
+    }
+
+	[HttpPost]
+	public IActionResult UpdateExperience(Experience experience)
+	{
+		var value = _context.Experiences.Update(experience);
+        _context.SaveChanges();
+		return RedirectToAction("ExperienceList");
+	}
+
 }
